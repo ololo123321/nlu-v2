f92432fc6547335ae345c8733bddd185a95a3db5
ololo123321 <vitkadylin@gmail.com>
2022-03-10 15:18:29 +0300

    WIP

---
+++ bin/train.sh
@@ -0,0 +1,10 @@
+experiment="coreference_resolution/mention_ranking"
+train_data_dir=""
+valid_data_dir=""
+output_dir=""
+
+python ../jobs/train.py \
+  +experiment=${experiment} \
+  train_data_dir=${train_data_dir} \
+  valid_data_dir=${valid_data_dir} \
+  output_dir=${output_dir}
