44b2f2f2855c8822d6d36a7698d552c712222f56
ololo123321 <vitkadylin@gmail.com>
2022-03-29 17:55:35 +0300

    save hydra configs to output_dir

--- bin/train.sh
+++ bin/train.sh
@@ -7,4 +7,5 @@ python ../jobs/train.py \
   +experiment=${experiment} \
   train_data_dir=${train_data_dir} \
   valid_data_dir=${valid_data_dir} \
-  output_dir=${output_dir}
+  output_dir=${output_dir} \
+  hydra.run.dir=${output_dir}
